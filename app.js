967099ecad506028f24569661ad2e488f27e8c1d
belqati <nahnahgames@gmail.com>
2018-09-12 16:57:14 -0500

    add GET request to library, use request in app.js; update html

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('live from app.js')
+// instantiate XHR via easyHTTP.js
+const http = new easyHTTP;
+
+// GET posts
+http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
+  // handle response
+  if(err){
+    console.log(err, 'Oops, something went wrong!');
+  } else {
+    console.log(posts);
+  }
+});
--- easyhttp.js
+++ easyhttp.js
@@ -1,1 +1,35 @@
-console.log('live from easyHTTP')
+function easyHTTP(){
+  // instantiate XHR
+  this.http = new XMLHttpRequest();
+}
+
+// CREATE PROTOTYPE METHODS
+// Make an HTTP GET Request
+easyHTTP.prototype.get = function(url, callback){
+  // async get request
+  this.http.open('GET', url, true);
+  // create this var for out-of-scope function
+  let self = this;
+  this.http.onload = function(){
+    // this.http is out of scope in function, use self variable
+    if(self.http.status === 200){
+      // use callback for async response
+      // include error handling via null for app.js
+      // console.log(self.http.responseText);
+      callback(null, self.http.responseText);
+    } else {
+      // error handling library side
+      callback('Error: ' + self.http.status);
+    }
+  }
+  // send the GET request
+  this.http.send();
+}
+
+// Make an HTTP POST Request
+
+
+// Make an HTTP PUT Request
+
+
+// Make an HTTP DELETE Request
